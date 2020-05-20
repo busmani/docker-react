e879ffe9357758001508e24a8519498046a15956
BadarUsmani <BadarUsmani@Badars-MacBook-Air.local>
2020-05-19 22:58:15 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          khushbo laga ke ajaoo!
+          My first app on cloud! Feature branch.
         </p>
         <a
           className="App-link"
